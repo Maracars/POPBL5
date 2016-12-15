04107ea45841a6bb657a5ff081bfaec72a9bb9b2
Joanes <joanes.plazaola@alumni.mondragon.edu>
2016-12-15 17:32:30 +0100

    Automatic scroll in console

--- WebContent/js/listener.js
+++ WebContent/js/listener.js
@@ -1,5 +1,7 @@
 var socket = io.connect("http://localhost:9092");
 
+var counter = 0;
+
 function onLoadFunction() {
 	var listenTo = $("#listenerRole").val();
 	if (listenTo === "") {
@@ -28,6 +30,10 @@ function onLoadFunction() {
 function render(data) {
 	var element = $("<li><a href = '#'><strong>" + data + "</strong></li>");
 	$(".console").append(element);
+	
+	$('.console').scrollTop($('.console li:last-child').position().top);
+
+	
 	/*
 	 * var notify = $.notify(`<strong>${data.id}</strong> ${data.izena}`, {
 	 * type: 'success', offset: { x: 50, y: 50 }, animate: { enter: 'animated
