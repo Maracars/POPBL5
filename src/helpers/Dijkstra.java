ea866a4d2025e634ae7a8ab12316b7354c69bcc6
Mikel Amutxastegi <mikelamutxastegi@MacBook-Air-de-Mikel.local>
2016-12-20 19:19:11 +0100

    Dijkstra algorithm implemented.

--- src/helpers/Dijkstra.java
+++ src/helpers/Dijkstra.java
@@ -1,63 +1,125 @@
 package helpers;
 
-import java.util.*;
-import java.util.stream.Stream;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
 
 public class Dijkstra {
 
-	public static void shortestPaths(List<Edge>[] graph, int s, int[] prio, int[] pred) {
-		int n = graph.length;
-		Arrays.fill(pred, -1);
-		Arrays.fill(prio, Integer.MAX_VALUE);
-		prio[s] = 0;
-		boolean[] visited = new boolean[n];
-		for (int i = 0; i < n; i++) {
-			int u = -1;
-			for (int j = 0; j < n; j++) {
-				if (!visited[j] && (u == -1 || prio[u] > prio[j]))
-					u = j;
-			}
-			if (prio[u] == Integer.MAX_VALUE)
-				break;
-			visited[u] = true;
+    private final List<Vertex> nodes;
+    private final List<Edge> edges;
+    private Set<Vertex> settledNodes;
+    private Set<Vertex> unSettledNodes;
+    private Map<Vertex, Vertex> predecessors;
+    private Map<Vertex, Integer> distance;
 
-			for (Edge e : graph[u]) {
-				int v = e.t;
-				int nprio = prio[u] + e.cost;
-				if (prio[v] > nprio) {
-					prio[v] = nprio;
-					pred[v] = u;
-				}
-			}
-		}
-	}
+    public Dijkstra(Graph graph) {
+            // create a copy of the array so that we can operate on this array
+            this.nodes = new ArrayList<Vertex>(graph.getVertexes());
+            this.edges = new ArrayList<Edge>(graph.getEdges());
+    }
 
-	static class Edge {
-		int t;
-		int cost;
+    public void execute(Vertex source) {
+            settledNodes = new HashSet<Vertex>();
+            unSettledNodes = new HashSet<Vertex>();
+            distance = new HashMap<Vertex, Integer>();
+            predecessors = new HashMap<Vertex, Vertex>();
+            distance.put(source, 0);
+            unSettledNodes.add(source);
+            while (unSettledNodes.size() > 0) {
+                    Vertex node = getMinimum(unSettledNodes);
+                    settledNodes.add(node);
+                    unSettledNodes.remove(node);
+                    findMinimalDistances(node);
+            }
+    }
 
-		public Edge(int t, int cost) {
-			this.t = t;
-			this.cost = cost;
-		}
-	}
+    private void findMinimalDistances(Vertex node) {
+            List<Vertex> adjacentNodes = getNeighbors(node);
+            for (Vertex target : adjacentNodes) {
+                    if (getShortestDistance(target) > getShortestDistance(node)
+                                    + getDistance(node, target)) {
+                            distance.put(target, getShortestDistance(node)
+                                            + getDistance(node, target));
+                            predecessors.put(target, node);
+                            unSettledNodes.add(target);
+                    }
+            }
 
-	// Usage example
-	public static void main(String[] args) {
-		int[][] cost = { { 0, 3, 2 }, { 0, 0, -2 }, { 0, 0, 0 } };
-		int n = cost.length;
-		@SuppressWarnings("unchecked")
-		List<Edge>[] graph = Stream.generate(ArrayList::new).limit(n).toArray(List[]::new);
-		for (int i = 0; i < n; i++) {
-			for (int j = 0; j < n; j++) {
-				if (cost[i][j] != 0) {
-					graph[i].add(new Edge(j, cost[i][j]));
-				}
-			}
-		}
-		int[] dist = new int[n];
-		int[] pred = new int[n];
-		shortestPaths(graph, 0, dist, pred);
+    }
 
-	}
+    private int getDistance(Vertex node, Vertex target) {
+            for (Edge edge : edges) {
+                    if (edge.getSource().equals(node)
+                                    && edge.getDestination().equals(target)) {
+                            return edge.getWeight();
+                    }
+            }
+            throw new RuntimeException("Should not happen");
+    }
+
+    private List<Vertex> getNeighbors(Vertex node) {
+            List<Vertex> neighbors = new ArrayList<Vertex>();
+            for (Edge edge : edges) {
+                    if (edge.getSource().equals(node)
+                                    && !isSettled(edge.getDestination())) {
+                            neighbors.add(edge.getDestination());
+                    }
+            }
+            return neighbors;
+    }
+
+    private Vertex getMinimum(Set<Vertex> vertexes) {
+            Vertex minimum = null;
+            for (Vertex vertex : vertexes) {
+                    if (minimum == null) {
+                            minimum = vertex;
+                    } else {
+                            if (getShortestDistance(vertex) < getShortestDistance(minimum)) {
+                                    minimum = vertex;
+                            }
+                    }
+            }
+            return minimum;
+    }
+
+    private boolean isSettled(Vertex vertex) {
+            return settledNodes.contains(vertex);
+    }
+
+    private int getShortestDistance(Vertex destination) {
+            Integer d = distance.get(destination);
+            if (d == null) {
+                    return Integer.MAX_VALUE;
+            } else {
+                    return d;
+            }
+    }
+
+    /*
+     * This method returns the path from the source to the selected target and
+     * NULL if no path exists
+     */
+    public LinkedList<Vertex> getPath(Vertex target) {
+            LinkedList<Vertex> path = new LinkedList<Vertex>();
+            Vertex step = target;
+            // check if a path exists
+            if (predecessors.get(step) == null) {
+                    return null;
+            }
+            path.add(step);
+            while (predecessors.get(step) != null) {
+                    step = predecessors.get(step);
+                    path.add(step);
+            }
+            // Put it into the correct order
+            Collections.reverse(path);
+            return path;
+    }
+
 }
