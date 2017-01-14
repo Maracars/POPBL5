2e86c00e93c9124b71cf80a0728d447766a54b28
Joanes <joanes.plazaola@alumni.mondragon.edu>
2017-01-14 01:17:01 +0100

    Graph created

--- WebContent/js/airlineCharts.js
+++ WebContent/js/airlineCharts.js
@@ -1,21 +1,48 @@
-$(document).ready(function() {
-	c3.generate({
-		bindto: '#barChart',
-		data : {
-			url : 'airline/weekFlightListJSON',
-			mimeType : 'json',
-			keys : {
-				x : 'planeName'
-			}
-		},
-		axis : {
-			x : {
-				type : 'category',
-				tick : {
-					count : 5,
-				}
-			}
-		}
-	});
+$(document).ready(
+		function() {
 
-});
+			$.ajax({
+				traditional : true,
+				type : "GET",
+				url : "weekFlightListJSON",
+				dataType : "json",
+				success : function(result, success) {
+					c3.generate({
+						bindto : '#barChart',
+						data : {
+							columns : [ [ 'week Flights', result.data.length,
+									50, 60 ] ]
+						},
+						axis : {
+							y : {
+								label : {
+									text : 'Week Flights',
+									position : 'outer-middle'
+								},
+								tick : {
+									count : 3,
+								// ADD
+								}
+							},
+
+							x : {
+								label : {
+
+									text : 'Weeks',
+									position : 'outer-middle'
+								},
+								type : 'category',
+								tick : {
+									count : 5,
+								}
+							}
+						}
+					});
+				},
+				error : function(xhr, ajaxOptions, thrownError) {
+					alert(xhr.status);
+					alert(thrownError);
+				}
+			});
+
+		});
